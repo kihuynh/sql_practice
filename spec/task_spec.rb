1b182319845ecce3e39a1da36ada9e98e606a88e
Kimberly Huynh and Logan Tanous <Guest@epicodus-8D.local>
2018-02-19 13:30:48 -0800

    everything worrrks

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,6 +1,7 @@
 require("rspec")
 require("pg")
 require("task")
+require('spec_helper')
 
 DB = PG.connect({:dbname => 'to_do_test'})
 
@@ -11,19 +12,39 @@ RSpec.configure do |config|
 end
 
 describe(Task) do
-  describe("#==") do
-    it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL"})
-      task2 = Task.new({:description => "learn SQL"})
-      expect(task1).to(eq(task2))
+  describe(".all") do
+    it("is empty at first") do
+      expect(Task.all()).to(eq([]))
     end
   end
+
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => "learn SQL"})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
   end
 
+  describe("#description") do
+    it("lets you read the description out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.description()).to(eq("learn SQL"))
+    end
+  end
+
+  describe("#list_id") do
+    it("lets you read the list ID out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
+  describe("#==") do
+    it("is the same task if it has the same description") do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(task1).to(eq(task2))
+    end
+  end
 end
